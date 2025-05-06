5b753f2002dee4449444a3c57abb92e69e33af7c
AalbatrossGuy <thexcelsiorisback@gmail.com>
2025-05-06 22:13:22 +0530

    Dropped flask-dropzone. Using maintained fork of dropzone.js

--- server.py
+++ server.py
@@ -1,29 +1,22 @@
 import secrets
 import os.path
 from werkzeug.utils import secure_filename
-from flask_dropzone import Dropzone 
-from flask import Flask, request, render_template
-from dotenv import load_dotenv 
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-dropzone = Dropzone(app)
 rootdir = os.path.abspath(os.path.dirname(__file__))
 print(rootdir)
 app.config["SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
 
-app.config.update(
-        UPLOAD_PATH=os.path.join(rootdir, f'{os.getenv("ROOT_DIRECTORY")}/'),
-        DROPZONE_ALLOWED_FILE_TYPE=f'{os.getenv("DROPZONE_ALLOWED_FILE_TYPE")}',
-        DROPZONE_UPLOAD_ON_CLICK=os.getenv("DROPZONE_UPLOAD_ON_CLICK"),
-        DROPZONE_INPUT_NAME=f"{os.getenv('DROPZONE_INPUT_NAME')}",
-        DROPZONE_UPLOAD_MULTIPLE=os.getenv("DROPZONE_UPLOAD_MULTIPLE"),
-        DROPZONE_MAX_FILE_SIZE=int(os.getenv("DROPZONE_MAX_FILE_SIZE")),
-)
+app.config.update(UPLOAD_PATH=os.path.join(rootdir, f'{os.getenv("ROOT_DIRECTORY")}/'))
+
 
 @app.get("/")
 def index():
+    print("upload.html loaded")
     return render_template("index.html")
 
 #TODO: create redirect for successful upload
@@ -32,11 +25,10 @@ def index():
 def upload():
     if request.method == "POST":
         for item in request.files:
-            if item.startswith('RAWFILE'):
+            print(request.files)
+            if item.startswith('myfiles'):
                 f = request.files.get(item)
                 print(secure_filename(f.filename))
                 print(os.path.join(app.config['UPLOAD_PATH'], secure_filename(f.filename)))
                 f.save(os.path.join(app.config['UPLOAD_PATH'], secure_filename(f.filename)))
     return render_template("upload.html")
-
-
