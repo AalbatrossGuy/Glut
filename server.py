c7c8e014e90cc928c1e95eacc53295bc006aca97
AalbatrossGuy <thexcelsiorisback@gmail.com>
2025-05-11 19:06:16 +0530

    Added video image thumbnail

--- server.py
+++ server.py
@@ -3,14 +3,15 @@ import os.path
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, url_for,  send_from_directory, Blueprint
 from dotenv import load_dotenv
-
+from moviepy import VideoFileClip
+import cv2
 load_dotenv()
 
 app = Flask(__name__)
 rootdir = os.path.abspath(os.path.dirname(__file__))
 print(rootdir)
 app.config["SECRET_KEY"] = str(secrets.SystemRandom().getrandbits(128))
-app.config["MAX_CONTENT_LENGTH"] = 800 * 1000 * 1000 # 512 MB limit
+app.config["MAX_CONTENT_LENGTH"] = 800 * 1000 * 1000 # 800 MB limit
 app.config["UPLOAD_PATH"] = os.getenv("UPLOAD_PATH")
 storage_directory = Blueprint('storage_directory', __name__, url_prefix="/gallery", static_folder="GlutRoot")
 app.register_blueprint(storage_directory)
@@ -21,6 +22,12 @@ def format_size(file_size):
     p = math.pow(1000, get_unit)
     final_size = round(file_size / p, 2)
     return f"{final_size} {UNIT[get_unit]}"
+
+def create_thumbnail(file) -> None:
+    video = VideoFileClip(f"GlutRoot/{file}")
+    filenoext = file.rsplit('.', 1)[0]
+    get_frame = video.get_frame(3)
+    cv2.imwrite(f"GlutRoot/thumbnails/{filenoext}.jpg", get_frame)
 
 @app.get("/")
 def index():
@@ -35,15 +42,15 @@ def upload():
             print(f"request.files = {request.files}")
             if file.startswith('myfile'):
                 get_file = request.files.get(file)
-                print(f"file = {get_file}")
-                print(get_file.filename)
+                file_extension = get_file.filename.rsplit('.', 1)[1].lower()
                 print(f"STREAM = {get_file.stream}")
                 start = time.time()
                 with open(f"{os.getenv('ROOT_DIRECTORY')}/{get_file.filename}", 'wb') as file_binary:
                     for file_chunk in get_file.stream:
-                        # print(f"CHUNK = {file_chunk}\n")
                         file_binary.write(file_chunk)
                 end = time.time()
+                if file_extension in ['mp4', 'mov']:
+                    create_thumbnail(get_file.filename)
                 print(f"TIME TAKEN = {end - start}s")
     return render_template("upload.html")
 
@@ -52,16 +59,32 @@ def upload():
 def gallery():
     files = sorted(os.listdir(app.config['UPLOAD_PATH']))
     file_meta = []
-    print(files)
+    # print(files)
     for file in files:
-        file_meta.append({
-            'name': file,
-            'path': url_for("storage_directory.static", filename=file),
-            'ext': file.rsplit('.', 1)[1].lower(),
-            'size': format_size(os.stat(f"GlutRoot/{file}").st_size),
-        })
-        print(file_meta)
+        if file == "thumbnails":
+            continue
+        if file.rsplit('.', 1)[1].lower() in ['mp4', 'mov']:
+            file_meta.append({
+                'name': file,
+                'path': url_for("storage_directory.static", filename=file),
+                'ext': file.rsplit('.', 1)[1].lower(),
+                'size': format_size(os.stat(f"GlutRoot/{file}").st_size),
+                'thumbnail': url_for("storage_directory.static", filename=f"thumbnails/{file.rsplit('.', 1)[0]}.jpg"), 
+            })
+        else:
+        # if not file.rsplit('.', 1)[1].lower() in ['mp4', 'mov']:
+            file_meta.append({
+                'name': file,
+                'path': url_for("storage_directory.static", filename=file),
+                'ext': file.rsplit('.', 1)[1].lower(),
+                'size': format_size(os.stat(f"GlutRoot/{file}").st_size),
+            })
     return render_template("gallery.html", files=file_meta)
+
+def test():
+    return 'SENDING FROM FUNC'
+
+app.jinja_env.globals.update(test=test)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=4000)
